5bc14a9080e334185e2972f43e3c207e74bc1c4b
Tommaso Ribaudo <tommasoribaudo1@gmail.com>
2021-08-17 21:27:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post('/statisticalCalculation', (req, res) => {
+    console.log('statisticalCalculation');
     let request = new projectModel(req.body);
     request.save((err, data) => {
         if (err) {
