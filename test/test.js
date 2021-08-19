e72e27e575c4162137b2ace87839b54fdb15cb3a
Tommaso Ribaudo <tommasoribaudo1@gmail.com>
2021-08-19 17:27:33 -0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -7,63 +7,79 @@ let server = require("../index");
 chai.use(chaiHttp);
 
 //testing post
-describe("Testing POST request", function() {
-    it("should return average, median, standardDeviation", function() {
-        try {
-            console.log("Testing POST request 1");
-            chai.request("http://localhost:3001/")
-                .post("/statisticalCalculation")
-                .set('content-type', 'application/x-www-form-urlencoded')
-                .send({
-                    list: [2, 3, 4, 5, 6, 7, 8, 9, 10]
-                })
-                .end(function(err, res) {
-                    console.log("end");
-                    console.log("error: " + err)
-                    console.log(res.body);
+describe("Testing POST request", () => {
+    it("should return average, standardDeviation", (done) => {
+        chai.request("http://localhost:3001")
+            .post("/statisticalCalculation")
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send({
+                list: [2, 3, 4, 5, 6, 7, 8, 9, 10]
+            })
+            .end(function(err, res) {
+
+                console.log('====================================');
+                console.log(res.body);
+                console.log('====================================');
+
+                if (err) {
+                    done(err);
+                } else {
                     res.should.have.status(200);
                     res.body.should.have.property("average");
-                    res.body.should.have.property("median");
+                    //res.body.should.have.property("median");
                     res.body.should.have.property("standardDeviation");
-                });
-        } catch (e) {
-            console.log(e);
-        }
+                    done();
+                }
+            });
     });
-    it("should return average, median, standardDeviation", function() {
+    it("should return average, standardDeviation", (done) => {
         console.log("Testing POST request 2");
-        chai.request("http://localhost:3001/")
+        chai.request("http://localhost:3001")
             .post("/statisticalCalculation")
             .set('content-type', 'application/x-www-form-urlencoded')
             .send({
                 list: [15, 15, 15, 15, 15, 15, 15, 15, 15]
             })
             .end(function(err, res) {
-                console.log("end");
+
+                console.log('====================================');
                 console.log(res.body);
-                res.should.have.status(200);
-                res.body.should.have.property("average");
-                res.body.should.have.property("median");
-                res.body.should.have.property("standardDeviation");
+                console.log('====================================');
+
+                if (err) {
+                    done(err);
+                } else {
+                    res.should.have.status(200);
+                    res.body.should.have.property("average");
+                    //res.body.should.have.property("median");
+                    res.body.should.have.property("standardDeviation");
+                    done();
+                }
 
             });
     });
-    it("should return average, median, standardDeviation", function() {
+    it("should return average, standardDeviation", (done) => {
         console.log("Testing POST request 3");
 
-        chai.request("http://localhost:3001/")
+        chai.request("http://localhost:3001")
             .post("/statisticalCalculation")
             .set('content-type', 'application/x-www-form-urlencoded')
             .send({
                 list: [0, 0, 0, -15, -15, 1, 2, 30148643251, 14]
             })
             .end(function(err, res) {
-                console.log("end");
-                console.log(res.body);
-                res.should.have.status(200);
-                res.body.should.have.property("average");
-                res.body.should.have.property("median");
-                res.body.should.have.property("standardDeviation");
+                if (err) {
+                    done(err);
+                } else {
+                    console.log('====================================');
+                    console.log(res.body);
+                    console.log('====================================');
+                    res.should.have.status(200);
+                    res.body.should.have.property("average");
+                    //res.body.should.have.property("median");
+                    res.body.should.have.property("standardDeviation");
+                    done();
+                }
             });
     });
 });
